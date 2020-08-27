f8d498635ccb425499107558a6c0800478d3f605
Gabriela da Rosa Neto <gabriela.netor94@gmail.com>
2020-08-27 16:01:51 -0300

    sistema de modulos

---
+++ moduloA.js
@@ -0,0 +1,5 @@
+console.log(this) //objeto vazio que  consegue adicionar novos atributos para este objeto
+
+this.ola = 'we are fine!'
+exports.helloNode = 'Welcome to the Node!'
+module.exports.seeYouLater = 'Exporting whatever modules'
---
+++ moduloCliente.js
@@ -0,0 +1,12 @@
+//acessar esses dois módulos
+const moduloA = require('./moduloA')
+const moduloB = require('./moduloB')
+
+console.log(moduloA.ola)
+console.log(moduloA.helloNode)
+console.log(moduloA.seeYouLater)
+console.log(moduloA)
+
+console.log(moduloB.bomDia)
+console.log(moduloB.boaNoite())
+console.log(moduloB)
